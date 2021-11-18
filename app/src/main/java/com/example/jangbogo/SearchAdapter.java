ce7ebb1fe6ffd033fdae30ec10d4cb6d8f10f259
zzung00 <lee759444@gmail.com>
2021-11-18 16:09:49 +0900

    update

--- app/src/main/java/com/example/jangbogo/SearchAdapter.java
+++ app/src/main/java/com/example/jangbogo/SearchAdapter.java
@@ -1,58 +1,78 @@
 package com.example.jangbogo;
 
 import android.content.Context;
-import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.ProgressBar;
-import android.widget.SearchView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchAdapter extends CursorAdapter {
-    private LayoutInflater layoutInflater;
-    private Context context;
-    private SearchView searchView;
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
     private List<SearchItem> searchItems = new ArrayList<>();
 
-    public SearchAdapter(Context context, Cursor c, SearchView s) {
-        super(context, c, false);
-        this.context = context;
-        this.searchView = s;
-        this.layoutInflater = LayoutInflater.from(context);
-    }
-
-    public void setSearchItems(List<SearchItem> searchItems) {
+    public SearchAdapter(List<SearchItem> searchItems) {
         this.searchItems = searchItems;
     }
 
+    @NonNull
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = layoutInflater.inflate(R.layout.activity_search_item, parent, false);
-        return view;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_search_item, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        String market = cursor.getString(cursor.getColumnIndexOrThrow("marketName"));
-        String product = cursor.getString(cursor.getColumnIndexOrThrow("productName"));
-        TextView marketName = (TextView) view.findViewById(R.id.marketNameInSearch);
-        marketName.setText(market);
-        TextView possessName = (TextView) view.findViewById(R.id.possessName);
-        possessName.setText(product);
-        TextView possibility = (TextView) view.findViewById(R.id.txtPossbility);
-        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        SearchItem searchItem = searchItems.get(position);
+        holder.marketNameInSearch.setText(searchItem.getMarket().getName());
 
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView txtMarket = (TextView) view.findViewById(R.id.marketNameInSearch);
-                searchView.setIconified(true);
+        if (searchItem.getStock() != null) {
+            holder.possessName.setText(searchItem.getStock().getProduct().getName());
+            if (searchItem.getStock().getCount() <= 30) {
+                holder.txtPossibility.setText("현재 재고 보유 가능성 낮음");
+                holder.progressBar.setProgress(30);
+                holder.progressBar.getIndeterminateDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+            }else if (searchItem.getStock().getCount() >= 31 || searchItem.getStock().getCount() <= 60){
+                holder.txtPossibility.setText("현재 재고 보유 가능성 있음");
+                holder.progressBar.setProgress(60);
+                holder.progressBar.getIndeterminateDrawable().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN);
+            } else {
+                holder.txtPossibility.setText("현재 재고 보유 가능성 높음");
+                holder.progressBar.setProgress(100);
+                holder.progressBar.getIndeterminateDrawable().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
             }
-        });
+        }
     }
+
+    @Override
+    public int getItemCount() {
+        return searchItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView marketNameInSearch;
+        TextView possessName;
+        TextView txtPossibility;
+        ProgressBar progressBar;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            marketNameInSearch = itemView.findViewById(R.id.marketNameInSearch);
+            possessName = itemView.findViewById(R.id.possessName);
+            txtPossibility = itemView.findViewById(R.id.txtPossbility);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/jangbogo/SearchResultActivity.java
@@ -0,0 +1,13 @@
+package com.example.jangbogo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
